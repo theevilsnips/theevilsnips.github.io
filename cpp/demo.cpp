1152141965ff8a2f0142c352162574a059282de9
theevilsnips <120704623+theevilsnips@users.noreply.github.com>
2025-12-15 03:21:41 +0000

    clanker stuff to learn c++

---
+++ cpp/demo.cpp
@@ -0,0 +1,6 @@
+// Launcher / instructions for the table tennis demo
+// Build and run:
+//   g++ -std=c++17 cpp/table_tennis.cpp -o tt && ./tt
+// Controls: 'w' = up, 's' = down, 'q' = quit
+
+int main() { return 0; }
